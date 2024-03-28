462bd4ba6ebd3eb023a8c264d9f5babe26fc148b
SLAC fphysics <zack@slac.stanford.edu>
2024-03-28 11:51:47 -0700

    typo fix, once more with feeling

--- F2_dashboard.py
+++ F2_dashboard.py
@@ -120,7 +120,7 @@ class F2_dashboard(Display):
         ind_LI18.setGeometry(0,0,92,26)
 
         if gethostname() not in ACR_WORKSTATIONS:
-            self.ui.start_matlab_server.SetEnabled(False)
+            self.ui.start_matlab_server.setEnabled(False)
 
         return
 
